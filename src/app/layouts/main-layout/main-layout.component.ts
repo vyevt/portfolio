6137df3e99486d9cf4e43e602906145c67da5e2b
Volodymyr Yevtushenko <wladimir.yetv@gmail.com>
2023-05-20 12:20:24 +0300

    navigate to named outlets by default

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-csgo-layout',
@@ -6,21 +7,45 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
-   cardActivate:boolean=false
-  taskActivate:boolean=false
-  toolbarActivate:boolean=false
-  visitsActivate:boolean=false
-  statsActivate:boolean=false
-  statisticsActivate:boolean=false
-  settingsActivate:boolean=false
- 
+  cardActivate: boolean = false
+  taskActivate: boolean = false
+  toolbarActivate: boolean = false
+  visitsActivate: boolean = false
+  statsActivate: boolean = false
+  statisticsActivate: boolean = false
+  settingsActivate: boolean = false
+
   ngOnInit(): void {
+    this.router.navigate([
+      {
+        outlets:
+        {
+          settings: 'settings'
+
+          ,
+          stats: 'stats'
+
+          ,
+          visits: 'visits'
+
+          ,
+          toolbar: 'toolbar_open'
+
+          ,
+          user_task: 'user-task'
+
+          ,
+          user_card: 'user-card'
+
+        }
+      }])
+    console.log(this.router)
   }
   constructor(
-    private cdRef: ChangeDetectorRef   
+    private cdRef: ChangeDetectorRef, private router: Router
   ) { }
 
   ngAfterViewInit(): void {
-     this.cdRef.detectChanges(); 
+    this.cdRef.detectChanges();
   }
 }
